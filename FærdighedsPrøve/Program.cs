041eab672e8799e95a338923792338bc947da8ed
Kevin Waweru <kew001@edu.zealand.dk>
2024-06-12 22:10:51 +0200

    Add project files.

---
+++ FærdighedsPrøve/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FærdighedsPrøve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
